5b96eb2323c5fd88651e0adca38ff81c5c38eb91
UmutcanElci <umutcanelci@yaani.com>
2022-08-16 13:01:18 +0300

    Object(Little fix for Inheritance)

--- src/Workout/Object/BasketballPlayer.java
+++ src/Workout/Object/BasketballPlayer.java
@@ -6,15 +6,19 @@ public class BasketballPlayer {
     //Bu veriler her yeni bir oyuncu girmek istediğimizde kullanıcı ya da bizim tarafından isteyeceğimiz ve girilmesi gereken veriler.
     String name;
     String surName;
-    String position;
+
     int jerseyNumber;
     double height;
     double wingspan;
     //Objeler alan oluşturlup tutulduğu gibi bir işlevide gerçekleştirebilir bunlara da method deriz.
     //Örnek olarak bir basketbol oyuncusunun yapıcağı bazı işlevleri örnek alalım.
+
+
     void shoot() {}
     void block(){}
     void rebound(){}
+
+
     //Burada verdiğim methodlarda alanlarımızı ya da başka türlü bizim gireceğimiz verileri kullanarak çalıştırabiliriz.
 
     /*Şimdi toparlıyalım elimizde bir classımız var bu classı bir obje oluşturmak için kullanıyoruz ve objemizi oluşturduğumuzda classımızın içinde
@@ -25,13 +29,22 @@ public class BasketballPlayer {
     //Constructors(Yapıcı denebilir) özel bir method dur ve görevi objemizi oluşturmak ve işlemektir basitçe.
     //Constructors oluştururken basit kurallar vardı ilki Constructors bir geri dönüş tipi yoktur ya da geri dönüştipi oluşturduğumuz classtır demek yanlış olmaz.
     //Son olarak da Constructors classımızın ismi ile aynı olmak zorunda dır.
-    public BasketballPlayer(String name, String surName, String position, int jerseyNumber){
+
+
+    public BasketballPlayer(String name, String surName,  int jerseyNumber){
         this.name = name;
         this.surName = surName;
-        this.position = position;
+
         this.jerseyNumber = jerseyNumber;
-        System.out.println(name+ " " + surName);
     }
+    public void printPlayer(){
+        System.out.println("Name: "+ name);
+        System.out.println("Surname: "+ surName);
+        System.out.println("Jersey Number: "+ jerseyNumber);
+    }
+
+
+
     //Yapıcımızı oluşturduk şimdi eğer biz bir obje oluşturmak istiyorsak classımız daki alan ve methodları çekmek için this anahtar kelimesini kullanırız.
     //this anahtar kelimesinin özelliği alanlarımız ve girilecek verilerin isimlerinin aynı olması diye biliriz şimdilik bu konuyu derinlice ilerde açıklıyacağım.
 
