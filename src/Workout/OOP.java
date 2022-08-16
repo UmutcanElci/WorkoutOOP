f334c71b531b8dceaa3bf953b9d8c404298737e8
UmutcanElci <umutcanelci@yaani.com>
2022-08-16 13:00:27 +0300

    OOP Workout

--- src/Workout/OOP.java
+++ src/Workout/OOP.java
@@ -1,10 +1,16 @@
 package Workout;
+import Workout.Inheritance.PointGuard;
 import Workout.Object.BasketballPlayer;
 public class OOP {
     public static void main(String[] args){
         //Obje oluşturma
-        BasketballPlayer player1 = new BasketballPlayer("Stephen","Curry","PG",30);
 
+        //BasketballPlayer player1 = new BasketballPlayer("Stephen","Curry","PG",30);
+
+        //Inheritance
+
+        PointGuard player = new PointGuard("Stephen","Curry",99,30);
+        player.printPointGuard();
     }
 
 }
