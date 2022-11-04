51aab2859619844f97fea6c2a054052835c54d75
Umutcan Elçi <umutcanelci@yaani.com>
2022-11-04 21:26:46 +0300

    Car

---
+++ src/Workout/Car/Sedan.java
@@ -0,0 +1,18 @@
+package Workout.Car;
+
+public class Sedan extends Car{
+    private int length;
+
+    public Sedan(int speed, double regularPrice, String color, int length) {
+        super(speed, regularPrice, color);
+        this.length = length;
+    }
+
+    public double getSalePrice(){
+        if(length >20){
+            return (super.getSalePrice() - (0.05* super.getSalePrice()));
+        }else {
+            return super.getSalePrice()*0.1;
+        }
+    }
+}
